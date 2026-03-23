f542e7fcaf09f2081d13873aab92bebac62450d1
Olumide Akinkunmi <mide.akinkunmi@gmail.com>
2026-03-23 13:22:52 +0000

    change

--- server.js
+++ server.js
@@ -8,11 +8,10 @@ const serverlessExpress = require("@vendia/serverless-express");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 app.use(cors());
 
 app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: false, limit: "10mb" }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.post("/api/extract-text", async (req, res) => {
